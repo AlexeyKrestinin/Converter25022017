92cd45eea4a7d169d9749b09e606630b11cfd241
Алексей Крестинин <alexeykrestinin@gmail.com>
2017-02-25 15:18:25 +0300

    implemented ConverterView

--- HelloSwift/ConverterView.swift
+++ HelloSwift/ConverterView.swift
@@ -8,14 +8,59 @@
 
 import UIKit
 
-class ConverterView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+// чтобы можно было иметь слабую ссылку 
+// зададим наследование протокола от протокола class
+protocol ConverterViewDelegate:class {
+        func converterView(view:ConverterView,convertForward value:String?)
+        
+        func converterView (view: ConverterView, convertBackward value:String?)
     }
-    */
 
+
+    class ConverterView: UIView {
+    // Слабая ссылка нужна, потому, что принято на делегата хранить слабую ссылку (а во view скорее всего 
+    // будет сильная) иначе, они всегда будут лежать в памяти
+       weak var delegate:ConverterViewDelegate?
+        var toValue:String? { // левое поле
+            
+            set {
+                toTextField?.text = newValue
+            }
+            get {
+                return toTextField?.text
+            }
+        }
+        var fromValue:String? { //правое поле
+            get {
+               return fromTextField?.text
+            }
+            set {
+                fromTextField?.text = newValue
+            }
+        }
+// private - чтобы не было видно другим эти строки ввода
+    @IBOutlet private weak var toTextField:UITextField?
+    @IBOutlet private weak var fromTextField:UITextField?
+        
+        
+        
+        @IBAction func forwardPressed() {
+        delegate?.converterView(view: self, convertForward: fromValue)
+        }
+        @IBAction func backwardPressed() {
+        delegate?.converterView(view: self, convertBackward: toValue)
+        }
+// закругление углов у view
+        required init?(coder aDecoder:NSCoder) {
+            super.init(coder:aDecoder)
+            setup()
+            
+        }
+        private func setup() {
+            self.layer.cornerRadius = 5
+            self.clipsToBounds = true
+            
+        }
+        
 }
